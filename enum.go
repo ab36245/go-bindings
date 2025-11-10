164c2824bba6e10502666425cf070af8f71b984f
Andrew Brown <ab36245@gmail.com>
2025-11-10 20:56:08 +1100

    Added EnumSlice

--- enum.go
+++ enum.go
@@ -45,6 +45,55 @@ func (b _enum[T]) Type() string {
 	return fmt.Sprintf("enum[%T]", *b.binding)
 }
 
+func EnumSlice[T any](mapping map[string]T, binding *[]T) Binding {
+	return &_enumSlice[T]{
+		binding: CheckNotNil(binding),
+		keys:    nil,
+		mapping: mapping,
+	}
+}
+
+type _enumSlice[T any] struct {
+	binding *[]T
+	keys    []string
+	mapping map[string]T
+}
+
+func (b *_enumSlice[T]) Assign(str string) error {
+	for part := range strings.SplitSeq(str, ",") {
+		key, value, err := _enumParse(b.mapping, part)
+		if err != nil {
+			return err
+		}
+		*b.binding = append(*b.binding, value)
+		b.keys = append(b.keys, key)
+	}
+	return nil
+}
+
+func (b _enumSlice[T]) IsZero() bool {
+	return len(*b.binding) == 0
+}
+
+func (b *_enumSlice[T]) Reset() {
+	*b.binding = nil
+}
+
+func (b _enumSlice[T]) String() string {
+	s := ""
+	for i, k := range b.keys {
+		if i > 0 {
+			s += ", "
+		}
+		s += k
+	}
+	return fmt.Sprintf("[%s]", s)
+}
+
+func (b _enumSlice[T]) Type() string {
+	return _enumType[T]() + "..."
+}
+
 func _enumParse[T any](mapping map[string]T, str string) (string, T, error) {
 	str = strings.TrimSpace(str)
 	for k, v := range mapping {
@@ -53,4 +102,8 @@ func _enumParse[T any](mapping map[string]T, str string) (string, T, error) {
 		}
 	}
 	return "", *new(T), fmt.Errorf("bad enum value %q", str)
+}
+
+func _enumType[T any]() string {
+	return fmt.Sprintf("enum[%T]", *new(T))
 }
